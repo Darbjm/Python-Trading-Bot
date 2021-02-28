e3d27e105e9ebbf21661c6f6b44bfb9e29cc4d78
James Darby <56593075+Darbjm@users.noreply.github.com>
2021-02-28 15:50:13 +0000

    stock frame

---
+++ pyrobot/stock_frame.py
@@ -0,0 +1,60 @@
+import numpy as np
+import pandas as pd
+
+from datetime import time
+from datetime import datetime
+from datetime import timezone
+
+from typing import List
+from typing import Dict
+from typing import Union
+
+from pandas.core.groupby import DataFrameGroupBy
+from pandas.core.window import RollingGroupby
+
+
+class StockFrame():
+
+    def __init__(self, data, List[dict]) -> None:
+        self._data = data
+        self._frame: pd.DataFame = self.create_frame()
+        self._symbol_groups: DataFrameGroupBy = None
+        self._symbol_rolling_groups: RollingGroupby
+
+    @property
+    def frame(self) = > pd.DataFrame:
+        return self._frame
+
+    @property
+    def _symbol_groups(self) -> DataFrameGroupBy:
+
+        self._symbol_groups = self._frame.groupby(
+            by='symbol',
+            as_index=False,
+            sort=True
+        )
+
+        return self._symbol_groups
+
+    def _symbol_rolling_groups(self, size: int) -> RollingGroupby:
+
+        if not self._symbol_groups:
+            self._symbol_groups
+
+        self._symbol_rolling_groups = self._symbol_groups.rolling(size)
+
+        return self._symbol_rolling_groups
+
+    def create_frame(self) -> pd.DataFrame:
+
+        # Make a data frame
+        price_df = pd.DataFrame(data=self._data)
+        price_df = self._parse_datetime_column(price_df=price_df)
+        price_df = self._set_multi_index(price=price_df)
+
+        return price_df
+
+    def _parse_datetime_column(self, price_df: pd.DataFrame) -> pd.DataFrame:
+
+        price_df['datetime'] = pd.to_datetime(
+            price_df['datetime'], unit='ms', origin='unix')
